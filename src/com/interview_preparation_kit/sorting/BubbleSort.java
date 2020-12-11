b61442f14aded27dd03523ea2ad9c94d2a8fd954
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2020-12-11 13:15:01 -0100

    Bubble Sort...

---
+++ src/com/interview_preparation_kit/sorting/BubbleSort.java
@@ -0,0 +1,41 @@
+package com.interview_preparation_kit.sorting;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+public class BubbleSort {
+
+    // Complete the countSwaps function below.
+    static void countSwaps(int[] a) {
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        countSwaps(a);
+
+        scanner.close();
+    }
+}
