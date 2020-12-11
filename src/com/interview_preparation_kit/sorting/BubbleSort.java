22aa25325f4451acd93824e981e220caeea5cf6f
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2020-12-11 13:50:00 -0100

    Bubble Sort Solution...

--- src/com/interview_preparation_kit/sorting/BubbleSort.java
+++ src/com/interview_preparation_kit/sorting/BubbleSort.java
@@ -11,15 +11,33 @@ import java.util.regex.*;
 
 public class BubbleSort {
 
+    static void swap(int[] a, int i, int j){
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
     // Complete the countSwaps function below.
     static void countSwaps(int[] a) {
+        int countSwap = 0;
+        boolean isSorted = false;
 
-
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < a.length - 1; i++) {
+                if (a[i] > a[i+1]) {
+                    swap(a, i, i + 1);
+                    countSwap++;
+                    isSorted = false;
+                }
+            }
+        }
+        System.out.println("Array is sorted in "+ countSwap +" swaps.\n"+"First Element: "+a[0]+"\n"+"Last Element: "+a[a.length-1]);
     }
 
     public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(new File("src/com/sample_test_cases/bubble_sort/input/input01.txt"));
 
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
