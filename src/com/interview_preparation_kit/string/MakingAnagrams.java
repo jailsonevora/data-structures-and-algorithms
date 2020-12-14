e6db8229a6b3cbda94f747e477e2ac7a3a19e73c
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2020-12-14 08:29:59 -0100

    Making Anagrams

--- src/com/interview_preparation_kit/string/MakingAnagrams.java
+++ src/com/interview_preparation_kit/string/MakingAnagrams.java
@@ -18,22 +18,34 @@ public class MakingAnagrams {
         Map<String,Integer> hsMp = new HashMap<>();
 
         for (int i = 0; i < a.length(); i++) {
-            String key = new String(a.substring(i, i + 1).toCharArray());
-            hsMp.put(key, hsMp.getOrDefault(key, 0)+1);
+            String keyA = new String(a.substring(i, i + 1).toCharArray());
+            hsMp.put(keyA, hsMp.getOrDefault(keyA, 0)+1);
         }
 
         for (int i = 0; i < b.length(); i++) {
-            String key = new String(b.substring(i, i + 1).toCharArray());
-            if (hsMp.containsKey(key))
-                hsMp.put(key, hsMp.get(key)-1);
+            String keyB = new String(b.substring(i, i + 1).toCharArray());
+            if (hsMp.containsKey(keyB)){
+                hsMp.put(keyB, hsMp.get(keyB)-1);
+            }
+            else
+                hsMp.put(keyB, hsMp.getOrDefault(keyB, 0)+1);
         }
 
-        return 1;
+        int count = 0;
+        for (Map.Entry str : hsMp.entrySet()){
+            Integer integer = (int) str.getValue();
+            if(integer < 0)
+                count = count + Math.abs(integer);//(hsMp.get(key) - (hsMp.get(key)*2) );
+            else if (integer >= 0)
+                count += integer;
+        }
+
+        return ( (a.length()+1) + (b.length()+1) ) - count;
     }
 
     public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(new File("src/com/sample_test_cases/making_anagrams/input/input00.txt"));
+        Scanner scanner = new Scanner(new File("src/com/sample_test_cases/making_anagrams/input/input01.txt"));
 
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
