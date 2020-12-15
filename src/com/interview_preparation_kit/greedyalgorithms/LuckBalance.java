f6dbead240476273c09f1c432ed8f4c44c0f163d
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2020-12-15 11:23:46 -0100

    Luck Balance Problem...

--- src/com/interview_preparation_kit/greedyalgorithms/LuckBalance.java
+++ src/com/interview_preparation_kit/greedyalgorithms/LuckBalance.java
@@ -1,4 +1,53 @@
 package com.interview_preparation_kit.greedyalgorithms;
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class LuckBalance {
+
+    // Complete the luckBalance function below.
+    static int luckBalance(int k, int[][] contests) {
+
+        return 1;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(new File("src/com/sample_test_cases/luck_balance/input/input00.txt"));
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[][] contests = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            String[] contestsRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 2; j++) {
+                int contestsItem = Integer.parseInt(contestsRowItems[j]);
+                contests[i][j] = contestsItem;
+            }
+        }
+
+        int result = luckBalance(k, contests);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+
 }
