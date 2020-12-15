9a69392a5a75621c4012856979e1f81f2092d15e
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2020-12-15 10:10:23 -0100

    Minimum Absolute Difference in an Array Problem...

--- src/com/interview_preparation_kit/greedyalgorithms/MinimumAbsoluteDifferenceInAnArray.java
+++ src/com/interview_preparation_kit/greedyalgorithms/MinimumAbsoluteDifferenceInAnArray.java
@@ -1,4 +1,47 @@
 package com.interview_preparation_kit.greedyalgorithms;
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class MinimumAbsoluteDifferenceInAnArray {
+
+    // Complete the minimumAbsoluteDifference function below.
+    static int minimumAbsoluteDifference(int[] arr) {
+
+        return 1;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(new File("src/com/sample_test_cases/minimum_absolute_difference_in_an_array/input/input00.txt"));
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = minimumAbsoluteDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
