b916a3ff95a736a629e63c807d30f9450490d8cd
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2020-12-23 15:08:53 -0100

    Detect a Cycle Solved with two pointers...

--- src/com/interview_preparation_kit/linkedlists/DetectACycle.java
+++ src/com/interview_preparation_kit/linkedlists/DetectACycle.java
@@ -29,4 +29,19 @@ public class DetectACycle {
 
         return false;
     }
+
+    boolean hasCycleWithTwoPointers(SinglyLinkedListNode head) {
+
+        if (head == null) return false;
+
+        SinglyLinkedListNode slow = head;
+        SinglyLinkedListNode fast = head.next;
+
+        while (slow != fast) {
+            if (fast == null || fast.next == null) return false;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
 }
