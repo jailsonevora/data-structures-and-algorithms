5701cf09cc0b5014a5b1dbaf27115561771bd93e
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2020-12-23 15:36:21 -0100

    Fibonacci Numbers...

--- src/com/interview_preparation_kit/recursion/FibonacciNumbers.java
+++ src/com/interview_preparation_kit/recursion/FibonacciNumbers.java
@@ -1,4 +1,20 @@
 package com.interview_preparation_kit.recursion;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
 public class FibonacciNumbers {
+
+    public static int fibonacci(int n) {
+        return 0;
+    }
+
+
+    public static void main(String[] args)  throws IOException {
+        Scanner scanner = new Scanner(new File("src/com/sample_test_cases/fibonacci_numbers/input/input04.txt"));
+        int n = scanner.nextInt();
+        scanner.close();
+        System.out.println(fibonacci(n));
+    }
 }
