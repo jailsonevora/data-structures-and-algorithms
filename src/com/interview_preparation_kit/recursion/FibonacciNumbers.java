bd68919038412bb84bad2c5ae36cb997d9f930f9
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2020-12-25 14:56:07 -0100

    FibonacciNumbers solved without memoization...

--- src/com/interview_preparation_kit/recursion/FibonacciNumbers.java
+++ src/com/interview_preparation_kit/recursion/FibonacciNumbers.java
@@ -7,12 +7,15 @@ import java.util.*;
 public class FibonacciNumbers {
 
     public static int fibonacci(int n) {
-        return 0;
+        if(n == 0 || n == 1)
+            return n;
+        else
+            return fibonacci(n-1) + fibonacci(n-2);
     }
 
 
     public static void main(String[] args)  throws IOException {
-        Scanner scanner = new Scanner(new File("src/com/sample_test_cases/fibonacci_numbers/input/input04.txt"));
+        Scanner scanner = new Scanner(new File("src/com/sample_test_cases/fibonacci_numbers/input/input06.txt"));
         int n = scanner.nextInt();
         scanner.close();
         System.out.println(fibonacci(n));
