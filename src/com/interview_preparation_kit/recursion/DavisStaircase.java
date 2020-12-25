2af5388e2bf90e87c25d2c9c0a070191feee9349
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2020-12-25 16:48:08 -0100

    Davis' Staircase...

--- src/com/interview_preparation_kit/recursion/DavisStaircase.java
+++ src/com/interview_preparation_kit/recursion/DavisStaircase.java
@@ -1,4 +1,42 @@
 package com.interview_preparation_kit.recursion;
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class DavisStaircase {
+
+    // Complete the stepPerms function below.
+    static int stepPerms(int n) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(new File(""));
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int s = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int sItr = 0; sItr < s; sItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int res = stepPerms(n);
+
+            bufferedWriter.write(String.valueOf(res));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
