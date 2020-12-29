d519cf314fd36d9410772a771f6b9e091da962fd
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2020-12-29 11:01:54 -0100

    Crossword Puzzle Scenario...

--- src/com/interview_preparation_kit/recursion/CrosswordPuzzle.java
+++ src/com/interview_preparation_kit/recursion/CrosswordPuzzle.java
@@ -1,4 +1,51 @@
 package com.interview_preparation_kit.recursion;
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class CrosswordPuzzle {
+
+    // Complete the crosswordPuzzle function below.
+    static String[] crosswordPuzzle(String[] crossword, String words) {
+
+        return new String[1];
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(new File("src/com/sample_test_cases/crossword_puzzle/input/input00.txt"));
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] crossword = new String[10];
+
+        for (int i = 0; i < 10; i++) {
+            String crosswordItem = scanner.nextLine();
+            crossword[i] = crosswordItem;
+        }
+
+        String words = scanner.nextLine();
+
+        String[] result = crosswordPuzzle(crossword, words);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(result[i]);
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+
 }
