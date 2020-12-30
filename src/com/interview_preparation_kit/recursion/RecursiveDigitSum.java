f782b5c76974779a8fc29acaac0862896539d711
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2020-12-30 12:12:33 -0100

    Recursive Digit Sum Scenario...

--- src/com/interview_preparation_kit/recursion/RecursiveDigitSum.java
+++ src/com/interview_preparation_kit/recursion/RecursiveDigitSum.java
@@ -9,4 +9,31 @@ import java.util.concurrent.*;
 import java.util.regex.*;
 
 public class RecursiveDigitSum {
+
+    // Complete the superDigit function below.
+    static int superDigit(String n, int k) {
+
+        return 0;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        String n = nk[0];
+
+        int k = Integer.parseInt(nk[1]);
+
+        int result = superDigit(n, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
