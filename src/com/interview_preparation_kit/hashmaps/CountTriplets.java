411f3a118c76b1a50f094bea7d478bf288fe53cb
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2020-12-31 14:16:25 -0100

    Count Triplets Problem Scenario...

--- src/com/interview_preparation_kit/hashmaps/CountTriplets.java
+++ src/com/interview_preparation_kit/hashmaps/CountTriplets.java
@@ -13,4 +13,32 @@ import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 public class CountTriplets {
+    // Complete the countTriplets function below.
+    static long countTriplets(List<Long> arr, long r) {
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nr = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(nr[0]);
+
+        long r = Long.parseLong(nr[1]);
+
+        List<Long> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Long::parseLong)
+                .collect(toList());
+
+        long ans = countTriplets(arr, r);
+
+        bufferedWriter.write(String.valueOf(ans));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
