70718dea734196c4398ea24419257ecc9ddceded
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-01-01 14:04:46 -0100

    Maximum Element Problem...

--- src/com/problem_solving_kit/stacks/MaximumElement.java
+++ src/com/problem_solving_kit/stacks/MaximumElement.java
@@ -4,4 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class MaximumElement {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
 }
