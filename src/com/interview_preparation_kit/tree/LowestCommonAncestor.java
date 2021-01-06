aa2163bbcfb8004f769b40cf41864ff154b2f30c
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-01-06 10:19:17 -0100

    Lowest Common Ancestor Scenario...

--- src/com/interview_preparation_kit/tree/LowestCommonAncestor.java
+++ src/com/interview_preparation_kit/tree/LowestCommonAncestor.java
@@ -1,4 +1,20 @@
 package com.interview_preparation_kit.tree;
 
+import java.util.*;
+import java.io.*;
+
+
 public class LowestCommonAncestor {
+    class Node {
+        com.interview_preparation_kit.tree.Node left;
+        com.interview_preparation_kit.tree.Node right;
+        int data;
+
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
 }
