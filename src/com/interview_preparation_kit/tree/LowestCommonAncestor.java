8e6763f1c93060ee2fd8f31239d5b10f43ef7b94
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-01-07 14:42:10 -0100

    Lowest Common Ancestor Big O Analysis -> O(depth of the tree)...

--- src/com/interview_preparation_kit/tree/LowestCommonAncestor.java
+++ src/com/interview_preparation_kit/tree/LowestCommonAncestor.java
@@ -5,6 +5,7 @@ import java.io.*;
 
 public class LowestCommonAncestor {
 
+    // O(depth of the tree)
     public static Node lca(Node root, int v1, int v2) {
         if(root.data > v1 && root.data > v2)
             return lca(root.left, v1, v2);
