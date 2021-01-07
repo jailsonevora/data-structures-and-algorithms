dac9d750e78dfd64003ee49e52ccc7980adb199b
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-01-07 15:22:19 -0100

    Is This a Binary Search Tree Scenario Solved...

--- src/com/interview_preparation_kit/tree/IsThisABinarySearchTree.java
+++ src/com/interview_preparation_kit/tree/IsThisABinarySearchTree.java
@@ -12,6 +12,14 @@ public class IsThisABinarySearchTree {
          }
     */
     boolean checkBST(Node root) {
-        return true;
+        return checkBST(root, Integer.MIN_VALUE,Integer.MAX_VALUE);
+    }
+
+    boolean checkBST(Node node, int min, int max) {
+        if (node == null)
+            return true;
+        return min < node.data && node.data < max &&
+                        checkBST(node.left, min, node.data) &&
+                        checkBST(node.right, node.data, max);
     }
 }
