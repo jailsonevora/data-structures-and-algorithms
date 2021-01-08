6cc1354c6ab1b4b563dd03d5fd564c73c0795b66
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-01-08 15:39:03 -0100

    Huffman Decoding Scenario...

--- src/com/interview_preparation_kit/tree/HuffmanDecoding.java
+++ src/com/interview_preparation_kit/tree/HuffmanDecoding.java
@@ -1,4 +1,39 @@
 package com.interview_preparation_kit.tree;
 
+import java.util.*;
+
+abstract class Node1 implements Comparable<Node1> {
+    public  int frequency; // the frequency of this tree
+    public  char data;
+    public  Node1 left, right;
+    public Node1(int freq) {
+        frequency = freq;
+    }
+
+    // compares on the frequency
+    public int compareTo(Node1 tree) {
+        return frequency - tree.frequency;
+    }
+}
+
+class HuffmanLeaf extends Node1 {
+
+
+    public HuffmanLeaf(int freq, char val) {
+        super(freq);
+        data = val;
+    }
+}
+
+class HuffmanNode extends Node1 {
+
+    public HuffmanNode(Node1 l, Node1 r) {
+        super(l.frequency + r.frequency);
+        left = l;
+        right = r;
+    }
+
+}
+
 public class HuffmanDecoding {
 }
