5be6f90078813c389be7ad603657b2a3deb67104
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-02-06 12:28:53 -0100

    Find the nearest scenario...

--- src/com/Pratice/kit/interview_preparation/graphs/FindNearestClone.java
+++ src/com/Pratice/kit/interview_preparation/graphs/FindNearestClone.java
@@ -10,4 +10,60 @@ import java.util.regex.*;
 
 
 public class FindNearestClone {
+
+    // Complete the findShortest function below.
+
+    /*
+     * For the unweighted graph, <name>:
+     *
+     * 1. The number of nodes is <name>Nodes.
+     * 2. The number of edges is <name>Edges.
+     * 3. An edge exists between <name>From[i] to <name>To[i].
+     *
+     */
+    static int findShortest(int graphNodes, int[] graphFrom, int[] graphTo, long[] ids, int val) {
+        // solve here
+        return 0;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] graphNodesEdges = scanner.nextLine().split(" ");
+        int graphNodes = Integer.parseInt(graphNodesEdges[0].trim());
+        int graphEdges = Integer.parseInt(graphNodesEdges[1].trim());
+
+        int[] graphFrom = new int[graphEdges];
+        int[] graphTo = new int[graphEdges];
+
+        for (int i = 0; i < graphEdges; i++) {
+            String[] graphFromTo = scanner.nextLine().split(" ");
+            graphFrom[i] = Integer.parseInt(graphFromTo[0].trim());
+            graphTo[i] = Integer.parseInt(graphFromTo[1].trim());
+        }
+
+        long[] ids = new long[graph_nodes];
+
+        String[] idsItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < graph_nodes; i++) {
+            long idsItem = Long.parseLong(idsItems[i]);
+            ids[i] = idsItem;
+        }
+
+        int val = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int ans = findShortest(graphNodes, graphFrom, graphTo, ids, val);
+
+        bufferedWriter.write(String.valueOf(ans));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
